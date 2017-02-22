d08d17bc6b3c067b3b6ca0506ae9b18c7c8fba36
Michael Tao <mtao@dgp.toronto.edu>
2017-02-22 13:12:40 -0500

    moving machines

--- src/lscm.cpp
+++ src/lscm.cpp
@@ -1,4 +1,10 @@
 #include "lscm.h"
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
+#include "vector_area_matrix.h"
+#include <igl/repdiag.h>
+#include <igl/eigs.h>
+#include <iostream>
 
 void lscm(
   const Eigen::MatrixXd & V,
@@ -7,4 +13,41 @@ void lscm(
 {
   // Replace with your code
   U = V.leftCols(2);
+
+  std::cout << "LSCM start" << std::endl;
+  int dim = V.rows();
+
+
+  Eigen::SparseMatrix<double> L;
+  igl::cotmatrix(V,F,L);
+
+  Eigen::SparseMatrix<double> M;
+  igl::massmatrix(V,F,igl::MASSMATRIX_TYPE_DEFAULT,M);
+
+  Eigen::SparseMatrix<double> VA;
+  vector_area_matrix(F,VA);
+
+  Eigen::SparseMatrix<double> LD(2*dim,2*dim);
+  igl::repdiag(L,2,LD);
+
+  Eigen::SparseMatrix<double> A = LD + VA;
+
+  Eigen::SparseMatrix<double> B(2*dim,2*dim);
+  igl::repdiag(M,2,B);
+
+  Eigen::MatrixXd EVec;
+  Eigen::VectorXd EVal;
+
+  /*
+  std::cout <<A << std::endl;
+  std::cout <<B << std::endl;
+  */
+
+  std::cout << "Eigs?" << std::endl;
+  igl::eigs(A,B,3,igl::EIGS_TYPE_SM,EVec,EVal);
+  std::cout << EVal.transpose() << std::endl;
+  std::cout << "LSCM end" << std::endl;
+  U.col(0) = EVec.col(2).head(dim);
+  U.col(1) = EVec.col(2).tail(dim);
+
 }
--- src/vector_area_matrix.cpp
+++ src/vector_area_matrix.cpp
@@ -1,6 +1,8 @@
 #include "vector_area_matrix.h"
 #include <vector>
 #include <iostream>
+#include <igl/boundary_loop.h>
+#include <igl/vector_area_matrix.h>
 
 
 // [a c] A [b d]^* = ad - bc
@@ -23,23 +25,27 @@ void vector_area_matrix(
 
   std::vector<Eigen::Triplet<double>> trips;
 
+  using Index = Eigen::MatrixXi::Scalar;
+  std::vector<Index> bnd_loop;
+  igl::boundary_loop(F,bnd_loop);
 
-  for(int t = 0; t < F.rows(); ++t) {
-      const auto tri = F.row(t);
-      for(int i = 0; i < 3; ++i) {
-          int a = i;
-          int b = (i+1)%3;
+  for(int i = 0; i < bnd_loop.size(); ++i) {
+      int a = bnd_loop[i];
+      int b = bnd_loop[(i+1)%bnd_loop.size()];
 
-          a = tri(a);
-          b = tri(b);
-          trips.emplace_back(a,b+dim,1);
-          trips.emplace_back(a+dim,b,-1);
+      trips.emplace_back(a,b+dim,1);
+      trips.emplace_back(a+dim,b,-1);
+      trips.emplace_back(b+dim,a,1);
+      trips.emplace_back(b,a+dim,-1);
 
-      }
   }
 
   A.setFromTriplets(trips.begin(),trips.end());
+  A = .5 * A;
 
 
+  igl::vector_area_matrix(F,A);
+  //std::cout << "real: " << std::endl;
+  //std::cout << 4*A << std::endl;
 }
 
