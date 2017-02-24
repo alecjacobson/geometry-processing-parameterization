6bde72ee9540b881d63734ce383dae84b957197a
Rahul Arora <arorar@cs.toronto.edu>
2017-02-24 14:14:38 -0500

    Minor

--- src/lscm.cpp
+++ src/lscm.cpp
@@ -6,6 +6,14 @@
 #include "igl/eigs.h"
 #include <Eigen/SVD>
 #include "igl/vector_area_matrix.h"
+#include <iostream>
+//#include <Eigen/Core>
+//#include <Eigen/SparseCore>
+//#include <SymGEigsSolver.h>
+//#include <MatOp/SparseCholesky.h>
+//#include <MatOp/SparseSymMatProd.h>
+//#include <MatOp/SparseRegularInverse.h>
+
 
 using namespace Eigen;
 
@@ -32,28 +40,34 @@ void lscm(
 
 	SparseMatrix<double> M(n, n), B(2 * n, 2 * n);
 
-	igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_VORONOI, M);
+	igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
 
 	igl::repdiag(M, 2, B);
 
 	MatrixXd eigVec;
 	VectorXd eigVal;
 
+	//std::cout << Q << B << std::endl;
+
 	igl::eigs(Q, B, 3, igl::EIGS_TYPE_SM, eigVec, eigVal);
 
-	VectorXd v = eigVec.col(2);
+	//Spectra::SparseSymMatProd<double> opQ(Q);
+	//Spectra::SparseCholesky<double> opB(B);
+
+	//Spectra::SymGEigsSolver<double, Spectra::SMALLEST_MAGN, Spectra::SparseSymMatProd<double>, Spectra::SparseCholesky<double>, Spectra::GEIGS_CHOLESKY> solver(&opQ, &opB, 3, 6);
+
+	//solver.init();
+	//int nconv = solver.compute(1000, 1e-10, Spectra::SMALLEST_MAGN);
+
+	//assert(solver.info() == Spectra::SUCCESSFUL || nconv >= 3);
+	//	eigVec = solver.eigenvectors();
+	//	eigVal = solver.eigenvalues();
 
 	U.resize(n, 2);
-	U.col(0) = v.head(n);
-	U.col(1) = v.tail(n);
+	U.col(0) = eigVec.col(2).head(n);
+	U.col(1) = eigVec.col(2).tail(n);
 
 	JacobiSVD <MatrixXd> svd(U, ComputeFullV);
 
-	Matrix2d W = svd.matrixV();
-	//Matrix2d R;
-	//R(0, 0) = R(1, 1) = W(0, 0);
-	//R(1, 0) = W(0, 1);
-	//R(0, 1) = -W(0, 1);
-
-	U = U * W;
+	U = U * svd.matrixV();
 }
