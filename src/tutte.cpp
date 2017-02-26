42679d47fa11f97437a79370622a6a69e7aff68a
Alex Tessier <alex.tessier@autodesk.com>
2017-02-26 18:30:16 -0500

    Tutte works...although beetle looks more deformed than the image in the assignment.  Camel and others for tutte work. Spent way too long on vector area....pushing it without all the test code...

--- src/tutte.cpp
+++ src/tutte.cpp
@@ -1,11 +1,101 @@
 #include "tutte.h"
 
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+
+#include <igl/cotmatrix.h>
+#include <igl/map_vertices_to_circle.h>
+#include <igl/boundary_loop.h>
+#include <igl/min_quad_with_fixed.h>
+#include <igl/setdiff.h>
+#include <igl/slice.h>
+#include <igl/slice_into.h>
+
+#include <vector>
+#include <ostream>
+
+void tutte2(
+    const Eigen::MatrixXd & V,
+    const Eigen::MatrixXi & F,
+    Eigen::MatrixXd & U );
+
+
 void tutte(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = V.leftCols(2);
+    // Replace with your code
+    U = V.leftCols(2); // projects onto xy plane with foldovers
+    
+    // min_U 1/2 tr( U^T L U )
+    // Can use cotmatrix for L
+    Eigen::SparseMatrix<double> L;
+    igl::cotmatrix( V, F, L );
+    
+    // can make w_ij = 1 / || v_i - v_j ||
+
+    // constrain the boundary to the outside of the circle.
+    //std::vector< std::vector<int> > boundaryVerts
+    // Note - we are assuming there's only 1 boundary that matters, the longest.
+    Eigen::VectorXi boundaryVerts;
+    igl::boundary_loop( F, boundaryVerts );
+
+    Eigen::MatrixXd boundaryUV;
+    igl::map_vertices_to_circle( V, boundaryVerts, boundaryUV );
+
+
+    //igl::min_quad_with_fixed( )
+    igl::min_quad_with_fixed_data<double> mqwf;
+    Eigen::VectorXd B = Eigen::VectorXd::Zero( V.rows(), 1 ); // linear term
+    Eigen::VectorXd Beq; // empty constraints
+    Eigen::SparseMatrix<double> Aeq;
+    igl::min_quad_with_fixed_precompute( (-L).eval(), boundaryVerts, Aeq, true, mqwf);
+
+    Eigen::VectorXd U1 = U.col(0), U2 = U.col(1);
+    igl::min_quad_with_fixed_solve( mqwf, B, boundaryUV.col(0), Beq, U1 );
+    igl::min_quad_with_fixed_solve( mqwf, B, boundaryUV.col(1), Beq, U2 );
+
+    U.col(0) = U1;
+    U.col(1) = U2;
+
 }
 
+void tutte2(
+    const Eigen::MatrixXd & V,
+    const Eigen::MatrixXi & F,
+    Eigen::MatrixXd & U)
+{
+/*    // cut from tutte....
+    Eigen::VectorXi allVerts;
+    igl::colon<int>( 0, V.rows()-1, allVerts );
+    
+    // make a list of verts to use:
+    // in - inside or free variable verts
+    // b - boundary or assigned verts (set on our outside circle)
+    Eigen::VectorXi in, IA;
+    igl::setdiff( allVerts, boundaryVerts, in, IA );
+    
+    // Construct and slice up Laplacian
+    Eigen::SparseMatrix<double> L_in_in, L_in_b;
+    igl::slice( L, in, in, L_in_in );
+    igl::slice( L, in, boundaryVerts, L_in_b );
+    
+    // Solve PDE - for U1 (u)
+    //Eigen::SimplicialLLT<Eigen::SparseMatrix<double > > solver( -L_in_in );
+    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double > > solver( -L_in_in );
+    Eigen::VectorXd u_in = solver.solve( L_in_b * boundaryUV.col(0) ); // L_in_b*bc
+    // slice into solution
+    Eigen::VectorXd U1 = U.col(0);
+    igl::slice_into( u_in, in, U1 );
+
+    Eigen::VectorXd v_in = solver.solve( L_in_b * boundaryUV.col(1) ); // L_in_b*bc
+    // slice into solution
+    Eigen::VectorXd U2 = U.col(1);
+    igl::slice_into( v_in, in, U2 );
+
+    U.col(0) = U1;
+    U.col(1) = U2;
+
+*/    
+}
--- src/vector_area_matrix.cpp
+++ src/vector_area_matrix.cpp
@@ -1,10 +1,95 @@
 #include "vector_area_matrix.h"
 
+
+
+#include <igl/boundary_facets.h>
+
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+
+#include <vector>
+
 void vector_area_matrix(
   const Eigen::MatrixXi & F,
   Eigen::SparseMatrix<double>& A)
 {
-  // Replace with your code
-  A.resize(F.maxCoeff()+1,F.maxCoeff()+1);
+    // Replace with your code
+    //A.resize(F.maxCoeff()+1,F.maxCoeff()+1);
+
+    // max number of vertices referenced by faces
+    const int n = F.maxCoeff() + 1;
+    
+    Eigen::MatrixXi E; // boundary edges
+    igl::boundary_facets( F, E );
+
+    
+    std::vector< Eigen::Triplet<double> > trips;
+
+    const int nEdges = E.rows();
+    for( int e=0; e < nEdges; ++e )
+    {
+        int first  = E( e, 0 );
+        int second = E( e, 1 );
+
+        // 0  0  0  1
+        // 0  0 -1  0
+        // 0 -1  0  0
+        // 1  0  0  0
+        trips.push_back( Eigen::Triplet<double>( first,      second + n,  1.0 ) );
+        trips.push_back( Eigen::Triplet<double>( first + n,  second,     -1.0 ) );
+        trips.push_back( Eigen::Triplet<double>( second,     first + n,  -1.0 ) );
+        trips.push_back( Eigen::Triplet<double>( second + n, first,       1.0 ) );
+    }
+
+    Eigen::SparseMatrix< double > mtx( 2*n, 2*n );
+    mtx.setFromTriplets( trips.begin(), trips.end() );
+
+    A = mtx * 0.5;
 }
 
+// Our determinant for a pair of vertices on a boundary edge has a
+// quartenary quadratic form.  If our 2 vertices are:
+// U_1u, U_1v and  U_2u, U_2v
+// we can stack these as {U_1u, U_1v, U_2u, U_2v}
+// To write out our quartenary form, we can say that
+// our points are { x, y, z, w }
+// We'll use the conventional a,b,c... etc to be the coeffients.
+// So our general quartenary quadratic would be:
+//
+// ax^2 + by^2 +cz^2 + dw^2 + exy + fxz + gxw + hyz +iyw + jzw
+//
+// When we write it all out, we get a matrix A for a pair of points
+// that looks like
+//
+// 2a  e  f  g
+// e  2b  h  i
+// f   h 2c  j
+// g   i  j 2d 
+//
+// Our determinant is: U_1u U_2v - U_2u U_1v
+// which correspond to:
+// xw - yz
+// or -> gxw - hyz
+//
+// so in our matrix we need g=1, h=-1
+// so A in this case becomes:
+//
+// 0  0  0  1
+// 0  0 -1  0
+// 0 -1  0  0
+// 1  0  0  0
+//
+// which we need to apply to each set of boundary edges.
+// So we need to put this matrix into A for each set of  edges.
+//
+// In a way, this looks like two 90 degree vector rotations
+//  0  1
+// -1  0
+//
+//  and
+//                             
+// 0 -1
+// 1  0
+//
+// So we do the first rotation, then the second for the boundary points.
+//   
