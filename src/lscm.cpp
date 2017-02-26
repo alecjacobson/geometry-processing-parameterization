6359cef41a0afb3f1109279b439464d8058c5be3
Rinat Abdrashitov <rindopuz23@gmail.com>
2017-02-26 02:40:47 -0500

    lscm not done

--- src/lscm.cpp
+++ src/lscm.cpp
@@ -1,10 +1,57 @@
 #include "lscm.h"
+#include "vector_area_matrix.h"
+#include "igl/massmatrix.h"
+#include "igl/repdiag.h"
+#include "igl/eigs.h"
+#include "igl/cotmatrix.h"
+
+using namespace Eigen;
+using namespace igl;
 
 void lscm(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = V.leftCols(2);
+    // Replace with your code
+	int nvert = V.rows();
+
+	// Computing Q matrix
+	
+	SparseMatrix<double> L;
+	cotmatrix(V, F, L);
+
+	SparseMatrix<double> KK(2 * nvert, 2 * nvert);
+	repdiag(L, 2, KK);
+	
+	SparseMatrix<double> A;
+	vector_area_matrix(F, A);
+	SparseMatrix<double> Q = KK - 2*A;
+	
+	// Computing constraint matrix
+
+	SparseMatrix<double> M(nvert, nvert);
+	massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+
+	SparseMatrix<double> B(2 * nvert, 2 * nvert);
+	repdiag(M, 2, B);
+
+	// Solving Generalized Eigenvalue problem
+
+	int k = 4;
+	MatrixXd sU;
+	VectorXd sS;
+	eigs(Q, B, k, igl::EIGS_TYPE_SM, sU, sS);
+
+	// Find non zero eigen value
+
+	double nonZeroValue;
+	for (int i = sS.size() - 1; i >= 0; --i)
+	{
+		if (sS[i] > 0) 
+		{
+			nonZeroValue = sS[i];
+		}
+	}
+	
 }
--- src/vector_area_matrix.cpp
+++ src/vector_area_matrix.cpp
@@ -1,10 +1,36 @@
 #include "vector_area_matrix.h"
+#include "igl/boundary_loop.h"
+
+using namespace Eigen;
+using namespace std;
 
 void vector_area_matrix(
   const Eigen::MatrixXi & F,
   Eigen::SparseMatrix<double>& A)
 {
-  // Replace with your code
-  A.resize(F.maxCoeff()+1,F.maxCoeff()+1);
+	// Replace with your code
+
+	int n = F.maxCoeff() + 1;
+	vector<Triplet<double>> coef;
+	vector<vector<int>> bndr;
+	igl::boundary_loop(F, bndr);
+
+	for (auto& b : bndr) 
+	{
+		int bsize = b.size();
+		for (int v = 0; v < bsize; ++v)
+		{
+			int v1 = b[v], v2 = b[(v + 1) % bsize];
+	
+			coef.push_back(Triplet<double>(v1, v2 + n, 0.25));
+			coef.push_back(Triplet<double>(v2, v1 + n, -0.25));
+			coef.push_back(Triplet<double>(v2 + n, v1, 0.25));
+			coef.push_back(Triplet<double>(v1 + n, v2, -0.25));			
+		}		
+	}
+	
+	A.resize(2 * n, 2 * n);
+	A.setZero();
+	A.setFromTriplets(coef.begin(), coef.end());
 }
 
