bacc3030f85b034310735c167d4fc14c308ad668
zero-impact <lawsonfulton@gmail.com>
2018-02-23 13:33:00 -0500

    done

--- src/tutte.cpp
+++ src/tutte.cpp
@@ -1,11 +1,25 @@
 #include "tutte.h"
 
+#include "igl/boundary_loop.h"
+#include "igl/map_vertices_to_circle.h"
+#include "igl/cotmatrix.h"
+#include "igl/min_quad_with_fixed.h"
+
 void tutte(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = V.leftCols(2);
-}
+  Eigen::VectorXi boundary_verts;
+  igl::boundary_loop(F, boundary_verts);
 
+  Eigen::SparseMatrix<double> L;
+  igl::cotmatrix(V, F, L);
+
+  Eigen::MatrixXd boundary_pos_on_circ;
+  igl::map_vertices_to_circle(V, boundary_verts, boundary_pos_on_circ);
+
+  Eigen::SparseMatrix<double> sparse_zero;
+ 	Eigen::VectorXd zero = Eigen::VectorXd::Zero(V.rows());
+  igl::min_quad_with_fixed(L, zero, boundary_verts, boundary_pos_on_circ, sparse_zero, zero, false, U);
+}
