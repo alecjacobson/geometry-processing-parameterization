c8b6b7265cc8b9464af2abc34599fbacb217c3a9
Michelle <m.arkhangorodsky@gmail.com>
2018-02-24 13:42:44 -0500

    Implemented tutte

--- src/tutte.cpp
+++ src/tutte.cpp
@@ -1,11 +1,32 @@
 #include "tutte.h"
-
+#include <igl/boundary_loop.h>
+#include <igl/map_vertices_to_circle.h>
+#include <igl/min_quad_with_fixed.h>
+#include <vector>
+#include <igl/cotmatrix.h>
 void tutte(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = V.leftCols(2);
-}
+  // Map boundary vertices to unit circle
+  U = Eigen::MatrixXd::Zero(V.rows(), 2);
+  Eigen::VectorXi bnd;
+  igl::boundary_loop(F,bnd); // vertices of boundary
+  Eigen::MatrixXd bnd_uv;
+  igl::map_vertices_to_circle(V,bnd,bnd_uv);
 
+  // Get Laplacian
+  Eigen::SparseMatrix<double> L;
+  igl::cotmatrix(V,F,L);
+  // Linear term is 0 except at known values
+  Eigen::VectorXd B = Eigen::VectorXd::Zero(V.rows(),1);
+
+  // Empty constraints
+  Eigen::VectorXd Beq;
+  Eigen::SparseMatrix<double> Aeq;
+  // Minimize energey trace( 0.5*U'*A*U + U'*B + constant )
+  igl::min_quad_with_fixed_data<double> tutte;
+  igl::min_quad_with_fixed_precompute(L, bnd, Aeq,false, tutte);
+  igl::min_quad_with_fixed_solve(tutte, B, bnd_uv, Beq, U);
+}
