1b43febb83e1590880d37e59e97ee623424345da
Zhiyuan <jerryz.cheng@mail.utoronto.ca>
2018-10-29 17:42:32 -0400

    submit

--- src/lscm.cpp
+++ src/lscm.cpp
@@ -1,10 +1,34 @@
 #include "lscm.h"
+#include "vector_area_matrix.h"
+#include <igl/cotmatrix.h>
+#include <igl/repdiag.h>
+#include <igl/massmatrix.h>
+#include <igl/eigs.h>
+#include <Eigen/SVD>
 
 void lscm(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  Eigen::MatrixXd & U)
+	const Eigen::MatrixXd & V,
+	const Eigen::MatrixXi & F,
+	Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = V.leftCols(2);
+	// construct Q and B
+	Eigen::SparseMatrix<double> L, L2, A, Q, M, B;
+	igl::cotmatrix(V, F, L);
+	igl::repdiag(L, 2, L2);
+	vector_area_matrix(F, A);
+	Q.resize(A.rows(), A.rows()); // A is 2#V x 2#V
+	Q = L2 - A;
+	igl::massmatrix(V, F, igl::MassMatrixType::MASSMATRIX_TYPE_DEFAULT, M);
+	igl::repdiag(M, 2, B);
+
+	// solve generalized Eigen value problem
+	Eigen::MatrixXd eigenVec;
+	Eigen::VectorXd eigenVal;
+	igl::eigs(Q, B, 3, igl::EigsType::EIGS_TYPE_SM, eigenVec, eigenVal);
+
+	U.resize(V.rows(), 2);
+	U << eigenVec.col(2);
+	// rotate
+	Eigen::JacobiSVD<Eigen::MatrixXd> svd(U.transpose() * U, Eigen::ComputeFullU | Eigen::ComputeFullV);
+	U = U * svd.matrixU();
 }
--- src/tutte.cpp
+++ src/tutte.cpp
@@ -1,11 +1,28 @@
 #include "tutte.h"
+#include <igl/cotmatrix.h>
+#include <igl/map_vertices_to_circle.h>
+#include <igl/boundary_loop.h>
+#include <igl/min_quad_with_fixed.h>
 
 void tutte(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  Eigen::MatrixXd & U)
+	const Eigen::MatrixXd & V,
+	const Eigen::MatrixXi & F,
+	Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = V.leftCols(2);
-}
+	Eigen::VectorXi boundary;
+	igl::boundary_loop(F, boundary);
 
+	Eigen::MatrixXd UV;
+	igl::map_vertices_to_circle(V, boundary, UV);
+
+	Eigen::SparseMatrix<double> L;
+	igl::cotmatrix(V, F, L);
+	
+	Eigen::SparseMatrix<double> Aeq;
+	Eigen::MatrixXd B, Beq;
+	B.resize(V.rows(), 2);
+	igl::min_quad_with_fixed(L, B, boundary, UV, Aeq, Beq, false, U);
+	
+	// igl::min_quad_with_fixed_precompute(L, boundary, Aeq, false, data);
+	// igl::min_quad_with_fixed_solve(data, B, UV, Beq, U);
+}
