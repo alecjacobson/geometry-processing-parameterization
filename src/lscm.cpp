66eb87e2893579f8cc1e545286a2ee851800333f
Adrian <adrian.she@alumni.ubc.ca>
2018-10-30 17:30:54 -0400

    fix lscm method weirdness

--- src/lscm.cpp
+++ src/lscm.cpp
@@ -18,7 +18,7 @@ void lscm(
   igl::repdiag(L, 2, L_d);
   Eigen::SparseMatrix<double> A;
   vector_area_matrix(F, A);
-  Eigen::SparseMatrix<double> Q = L_d - A;
+  Eigen::SparseMatrix<double> Q = L_d  - 2.0 * A;
 
   // Construct the matrix B = (M 0; 0 M) where M is the mass matrix
   Eigen::SparseMatrix<double> M;
@@ -32,7 +32,7 @@ void lscm(
   igl::eigs(Q, B, 5, igl::EigsType::EIGS_TYPE_SM, eigen_vecs, eigen_vals);
   
   int idx = 1; // the second eigenvector calculated should be the Fielder vector
-  double tol = 1e-4; // Pick an eigenvector sufficiently away from 0
+   double tol = 1e-8; // Pick an eigenvector sufficiently away from 0
   // hope that an eigen_vec with non-zero eigen_val has been found among the calculated evals
   // otherwise just take the largest evec with the largest evec. 
   while (abs(eigen_vals[idx]) <= tol) {
@@ -52,6 +52,6 @@ void lscm(
   Eigen::Matrix2d covar = U.transpose() * U;
   Eigen::JacobiSVD<Eigen::Matrix2d> svd(covar, Eigen::ComputeFullU);
   for (int i = 0; i < U.rows(); i++) {
-	U.row(i) = U.row(i) * svd.matrixU().transpose(); 
+	U.row(i) = U.row(i) * svd.matrixU(); 
   }
 }
--- src/vector_area_matrix.cpp
+++ src/vector_area_matrix.cpp
@@ -6,30 +6,33 @@ void vector_area_matrix(
   Eigen::SparseMatrix<double>& A)
 {
   int V_size = F.maxCoeff()+1;
-  A.resize(V_size*2,V_size*2);
+  Eigen::SparseMatrix<double> A_u;
+  A_u.resize(V_size*2,V_size*2);
 
   // Determine the longest boundary loop
-  Eigen::VectorXi bdry;
-  igl::boundary_loop(F, bdry);
+  std::vector<std::vector<int>> bdry_loops;
+  igl::boundary_loop(F, bdry_loops);
   
   // Each edge (i, j) in the loop contributes 1/2 (x_i y_j - x_j y_i) to the vector area
   typedef Eigen::Triplet<double> T;
   std::vector<T> triples;
-  triples.reserve(bdry.size() * 4);
+  triples.reserve(bdry_loops[0].size() * 4);
+  for (int l = 0; l < bdry_loops.size() ; l++) {
+  std::vector<int> bdry = bdry_loops[l];
   for (int i = 0; i < bdry.size(); i++) {
 	if (i == bdry.size() - 1) {
-		triples.push_back(T(bdry[i], bdry[0] + V_size, 0.25));
-		triples.push_back(T(bdry[0] + V_size, bdry[i], 0.25));
-   		triples.push_back(T(bdry[0], bdry[i] + V_size, -0.25));
-   		triples.push_back(T(bdry[i] + V_size, bdry[0], -0.25));
+		triples.push_back(T(bdry[i], bdry[0] + V_size, 0.5));
+   		triples.push_back(T(bdry[i] + V_size, bdry[0], -0.5));
 	} 
 	else {
-		triples.push_back(T(bdry[i], bdry[i+1] + V_size, 0.25));
-		triples.push_back(T(bdry[i+1] + V_size, bdry[i], 0.25));
-   		triples.push_back(T(bdry[i+1], bdry[i] + V_size, -0.25));
-   		triples.push_back(T(bdry[i] + V_size, bdry[i+1], -0.25));
+		triples.push_back(T(bdry[i], bdry[i+1] + V_size, 0.5));
+   		triples.push_back(T(bdry[i] + V_size, bdry[i+1], -0.5));
 	}
   }
-  A.setFromTriplets(triples.begin(), triples.end());
+}
+  A_u.setFromTriplets(triples.begin(), triples.end());
+  Eigen::SparseMatrix<double> B;
+  B = A_u.transpose();
+  A = (A_u + B) / 2;
 }
 
