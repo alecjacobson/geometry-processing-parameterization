4821cf4d2377122139814b4c0e04d2a330dd94a1
liupeiLu <liupei.lu@mail.utoronto.ca>
2018-10-30 01:30:05 -0400

    Liupei Lu HW05

--- src/tutte.cpp
+++ src/tutte.cpp
@@ -1,4 +1,9 @@
 #include "tutte.h"
+#include <igl/boundary_loop.h>
+#include <igl/min_quad_with_fixed.h>
+#include <igl/map_vertices_to_circle.h>
+
+typedef Eigen::Triplet<double> T;
 
 void tutte(
   const Eigen::MatrixXd & V,
@@ -6,6 +11,48 @@ void tutte(
   Eigen::MatrixXd & U)
 {
   // Replace with your code
-  U = V.leftCols(2);
+  //U = V.leftCols(2);
+  int v = F.maxCoeff()+1;
+
+  //B in vector form for mapping vertices to circle
+  Eigen::VectorXi B;
+  igl::boundary_loop(F,B);
+  std::vector<T> tlist;
+
+  Eigen::SparseMatrix<double> L, Aeq;
+  L.resize(v,v);
+
+  //uniform laplacian
+  for (int i = 0; i < F.rows(); i++){
+    int v1 = F(i,0);
+    int v2 = F(i,1);
+    int v3 = F(i,2);
+
+    //adjacent -1
+    tlist.push_back(T(v1,v2,-1));
+    tlist.push_back(T(v2,v1,-1));
+    tlist.push_back(T(v1,v3,-1));
+    tlist.push_back(T(v3,v1,-1));
+    tlist.push_back(T(v2,v3,-1));
+    tlist.push_back(T(v3,v2,-1));
+
+    //diagonal degree 2
+    tlist.push_back(T(v1,v1, 2));
+    tlist.push_back(T(v2,v2, 2));
+    tlist.push_back(T(v3,v3, 2));
+  }
+
+  L.setFromTriplets(tlist.begin(),tlist.end());
+
+  Eigen::MatrixXd C;
+  igl::map_vertices_to_circle(V,B,C);
+
+  //solve U
+  igl::min_quad_with_fixed_data<double> data;
+  igl::min_quad_with_fixed_precompute(L, B, Aeq, false, data);
+
+  Eigen::MatrixXd Beq, tempB;
+  tempB.setZero(v,2);
+  igl::min_quad_with_fixed_solve(data, tempB, C, Beq, U);
 }
 
--- src/vector_area_matrix.cpp
+++ src/vector_area_matrix.cpp
@@ -1,4 +1,7 @@
 #include "vector_area_matrix.h"
+#include "igl/boundary_loop.h"
+
+typedef Eigen::Triplet<double> T;
 
 void vector_area_matrix(
   const Eigen::MatrixXi & F,
@@ -7,5 +10,29 @@ void vector_area_matrix(
   // Replace with your code
   int V_size = F.maxCoeff()+1;
   A.resize(V_size*2,V_size*2);
+  std::vector<std::vector<int>> B;
+  igl::boundary_loop(F,B);
+  std::vector<T> tlist;
+
+  for(int i = 0; i < B.size(); i++)
+  {
+    for(int j = 0; j < B[i].size(); j++){
+    //Get adjacent
+    int k = j+1;
+    //Prevent out of range
+    if (j == B[i].size() - 1){
+      k = 0;
+    }
+    tlist.push_back(T(B[i][j], B[i][k] + V_size, 0.5));
+    tlist.push_back(T(B[i][k], B[i][j] + V_size, -0.5));
+    }
+  }
+
+  //Symmetric
+  A.setFromTriplets(tlist.begin(), tlist.end());
+  Eigen::SparseMatrix<double> AT;
+  AT = A.transpose();
+  A = A + AT;
+  A = A * 0.5;
 }
 
