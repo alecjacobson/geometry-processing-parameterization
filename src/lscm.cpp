732b11bf25dc65fe46e57052de5efd8c03ade5ba
juliagilenko <jugilenko@gmail.com>
2018-10-30 23:39:27 -0400

    Implemented Tutte embedding and least squares conformal mapping, but something is definitely busted.

--- src/lscm.cpp
+++ src/lscm.cpp
@@ -1,10 +1,29 @@
 #include "lscm.h"
+#include "vector_area_matrix.h"
+#include <igl/cotmatrix.h>
+#include <igl/eigs.h>
+#include <igl/massmatrix.h>
+#include <igl/repdiag.h>
 
 void lscm(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  Eigen::MatrixXd & U)
+    const Eigen::MatrixXd & V,
+    const Eigen::MatrixXi & F,
+    Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = V.leftCols(2);
+    Eigen::SparseMatrix<double> L, A, Q, M, B;
+    igl::cotmatrix(V, F, L);
+    vector_area_matrix(F, A);
+
+    igl::massmatrix(V, F, igl::MassMatrixType::MASSMATRIX_TYPE_DEFAULT, M);
+    Q = igl::repdiag(L, 2) - A;
+    B = igl::repdiag(M, 2);
+
+    Eigen::MatrixXd sU, unrotated;
+    Eigen::VectorXd sS;
+    igl::eigs(Q, B, 3, igl::EIGS_TYPE_SM, sU, sS);
+
+    U.resize(sU.rows() / 2, 2);
+    Eigen::VectorXd u = sU.col(2) / sU.col(2).maxCoeff();
+    U.col(0) = u.head(sU.rows() / 2);
+    U.col(1) = u.tail(sU.rows() / 2);
 }
--- src/vector_area_matrix.cpp
+++ src/vector_area_matrix.cpp
@@ -1,11 +1,42 @@
 #include "vector_area_matrix.h"
+#include <igl/boundary_loop.h>
 
 void vector_area_matrix(
-  const Eigen::MatrixXi & F,
-  Eigen::SparseMatrix<double>& A)
+    const Eigen::MatrixXi & F,
+    Eigen::SparseMatrix<double>& A)
 {
-  // Replace with your code
-  int V_size = F.maxCoeff()+1;
-  A.resize(V_size*2,V_size*2);
+    int V_size = F.maxCoeff() + 1;
+    std::vector<std::vector<int>> boundaries;
+    igl::boundary_loop(F, boundaries);
+
+    typedef Eigen::Triplet<double> T;
+    std::vector<T> triplets;
+    int size = 0;
+
+    for (int i = 0; i < boundaries.size(); i++) 
+    {
+        size += (boundaries[i]).size();
+    }
+
+    triplets.reserve(4 * size);
+
+    for (int i = 0; i < boundaries.size(); i++) {
+        std::vector<int> b = boundaries[i];
+
+        for (int j = 0; j < b.size(); j++)
+        {
+            int v1 = b[j];
+            int v2 = b[(j + 1) % b.size()];
+
+            triplets.push_back(T(V_size + v2, v1, 0.25));
+            triplets.push_back(T(v2, V_size + v1, -0.25));
+
+            triplets.push_back(T(v1, V_size + v2, 0.25));
+            triplets.push_back(T(V_size + v1, v2, -0.25));
+        }
+    }
+
+    A.resize(V_size * 2, V_size * 2);
+    A.setFromTriplets(triplets.begin(), triplets.end());
 }
 
